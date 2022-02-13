964e9c121cea7636083c31f0edcdc28c06b5e504
Edivaldo <edivaldoluisb16@gmail.com>
2022-02-13 18:53:06 +0000

    pagina administrativa

--- clinicas/admin.py
+++ clinicas/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Produto, Encomenda, Agendamento, Blog
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco')
+
+
+class EncomendaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'telefone', 'morada')
+
+
+class AgendamentoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'telefone', 'data_agendada')
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cabecalho', 'texto_span')
+
+
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Encomenda, EncomendaAdmin)
+admin.site.register(Agendamento, AgendamentoAdmin)
+admin.site.register(Blog, BlogAdmin)
--- clinicas/models.py
+++ clinicas/models.py
@@ -6,31 +6,43 @@ from django.utils import timezone
 
 class Produto(models.Model):
     nome = models.CharField(max_length=250)
-    preco = models.FloatField
+    preco = models.FloatField(default=0.0)
     data_criacao = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.nome
 
 
 class Encomenda(models.Model):
     nome = models.CharField(max_length=250)
-    telefone = models.PositiveIntegerField
-    distrito = models.CharField
-    morada = models.CharField  # localidade
-    email = models.EmailField
+    telefone = models.PositiveIntegerField(blank=True, null=True)
+    distrito = models.CharField(max_length=20, blank=True)
+    morada = models.CharField(max_length=300, default='Sem morada')  # localidade
+    email = models.EmailField(blank=True)
     lista_produtos = models.TextField(verbose_name='Lista de produtos')
-    total = models.PositiveSmallIntegerField(verbose_name='Valor total da compra')
+    total = models.PositiveSmallIntegerField(verbose_name='Valor total da compra')  # editable=False
+
+    def __str__(self):
+        return self.nome
 
 
 class Agendamento(models.Model):
     nome = models.CharField(max_length=250)
-    idade = models.PositiveSmallIntegerField
-    telefone = models.PositiveIntegerField
+    idade = models.PositiveSmallIntegerField(null=True, blank=True)
+    telefone = models.PositiveIntegerField(blank=True, null=True)
     # email = models.EmailField
     data_agendada = models.DateTimeField(default=timezone.now, verbose_name='Data agendada')
     data_criacao = models.DateTimeField(default=timezone.now,
                                         verbose_name='Data da criacao ')
 
+    def __str__(self):
+        return self.nome
+
 
 class Blog(models.Model):
     cabecalho = models.CharField(max_length=300)
-    corpo = models.TextField
-    texo_span = models.CharField(max_length=100)
+    corpo = models.TextField(blank=True)
+    texto_span = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.cabecalho
