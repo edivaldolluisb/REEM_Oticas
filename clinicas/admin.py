eee5a579a61961f67db857cc8ee19ae83c5dc965
Edivaldo <edivaldoluisb16@gmail.com>
2022-02-13 19:01:42 +0000

    Update admin.py

--- clinicas/admin.py
+++ clinicas/admin.py
@@ -6,19 +6,23 @@ from .models import Produto, Encomenda, Agendamento, Blog
 
 class ProdutoAdmin(admin.ModelAdmin):
     list_display = ('nome', 'preco')
+    list_per_page = 30
 
 
 class EncomendaAdmin(admin.ModelAdmin):
     list_display = ('nome', 'telefone', 'morada')
     list_display_links = ('nome', 'telefone')
+    list_per_page = 30
 
 
 class AgendamentoAdmin(admin.ModelAdmin):
     list_display = ('nome', 'telefone', 'data_agendada')
+    list_per_page = 30
 
 
 class BlogAdmin(admin.ModelAdmin):
     list_display = ('id', 'cabecalho', 'texto_span')
+    list_per_page = 30
 
 
 admin.site.register(Produto, ProdutoAdmin)
