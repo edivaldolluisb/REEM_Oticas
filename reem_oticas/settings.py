7e2e56304e1b199dd3db2057728c1e09bf84d0d2
Edivaldo <edivaldoluisb16@gmail.com>
2022-02-18 14:56:28 +0000

    ferrou

--- reem_oticas/settings.py
+++ reem_oticas/settings.py
@@ -117,6 +117,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+# configuração para heroku
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# fim da configuração
+
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
@@ -127,14 +133,7 @@ STATICFILES_DIRS = [
 ]
 # configuração para guardar as imagens
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'media/'
-
-
-# configuração para heroku
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-# fim da configuração
-
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
