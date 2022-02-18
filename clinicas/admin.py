f4b5eecb6a6641298e5c14754039a7f1a510342b
Edivaldo <edivaldoluisb16@gmail.com>
2022-02-18 00:37:07 +0000

    adicionei boolean field +disponivél

--- clinicas/admin.py
+++ clinicas/admin.py
@@ -7,23 +7,27 @@ from .models import Produto, Encomenda, Agendamento, Blog
 class ProdutoAdmin(admin.ModelAdmin):
     list_display = ('nome', 'preco')
     list_per_page = 30
+    search_fields = ('nome', 'preco')
 
 
 class EncomendaAdmin(admin.ModelAdmin):
     list_display = ('nome', 'telefone', 'morada', 'entregue')
     list_display_links = ('nome', 'telefone')
     list_per_page = 30
+    search_fields = ('nome', 'telefone', 'morada')
 
 
 class AgendamentoAdmin(admin.ModelAdmin):
     list_display = ('nome', 'telefone', 'data_agendada', 'finalizado')
     list_per_page = 30
+    search_fields = ('nome', 'telefone', 'data_agendada',)
 
 
 class BlogAdmin(admin.ModelAdmin):
     list_display = ('id', 'cabecalho', 'texto_span')
     list_display_links = ('id', 'cabecalho')
     list_per_page = 30
+    search_fields = ('cabecalho', 'texto_span')
 
 
 admin.site.register(Produto, ProdutoAdmin)
--- clinicas/models.py
+++ clinicas/models.py
@@ -9,6 +9,7 @@ class Produto(models.Model):
     preco = models.FloatField(default=0.0)
     imagem = models.ImageField(blank=True)
     data_criacao = models.DateTimeField(default=timezone.now, blank=True)
+    disponivel = models.BooleanField(default=False)
 
     def __str__(self):
         return self.nome
