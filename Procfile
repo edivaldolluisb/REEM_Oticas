2be12714456133d27d90dac07a6fed5f71d391f1
Edivaldo <edivaldoluisb16@gmail.com>
2022-02-18 02:39:09 +0000

    first commit for heroku host

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn reem_oticas.wsgi --log-file -
--- reem_oticas/settings.py
+++ reem_oticas/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-13f)$vv4l5h@)l!^%ztqpll$kr)qnj4z@5v*@9h$yjgp3pu9p+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rem-oticas.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
