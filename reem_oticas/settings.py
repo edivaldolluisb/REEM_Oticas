87d691bf77f1a3f3b268465203064eefd09613b0
Edivaldo <edivaldoluisb16@gmail.com>
2022-02-18 03:48:39 +0000

    Update settings.py

--- reem_oticas/settings.py
+++ reem_oticas/settings.py
@@ -120,7 +120,7 @@ USE_L10N = True
 USE_TZ = True
 
 # configuração para heroku
-STATIC_ROOT = MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # fim da configuração
 
